ed9896fca8b2c9e9d306c4179630477ac09040c6
shredder42 <atbinder42@gmail.com>
2025-07-16 15:29:59 -0500

    backgrounds

--- client/main.go
+++ client/main.go
@@ -1,30 +1,72 @@
 package main
 
 import (
+	// "fmt"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	BackgroundImg          *ebiten.Image
+	BackgroundBuildingsImg *ebiten.Image
+}
+
+const (
+	screenWidth  = 640
+	screenHeight = 480
+)
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+
+	backgroundImgWidth := g.BackgroundImg.Bounds().Dx()
+	backgroundImgHeight := g.BackgroundImg.Bounds().Dy()
+
+	scaleX := float64(screenWidth) / float64(backgroundImgWidth)
+	scaleY := float64(screenHeight) / float64(backgroundImgHeight)
+
+	// fmt.Println(scaleX)
+
+	opts := ebiten.DrawImageOptions{}
+
+	opts.GeoM.Scale(scaleX, scaleY)
+
+	screen.DrawImage(g.BackgroundImg, &opts)
+	screen.DrawImage(g.BackgroundBuildingsImg, &opts)
+
+	opts.GeoM.Reset()
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return 640, 480
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Space Invasion!")
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+	backgroundImg, _, err := ebitenutil.NewImageFromFile("assets/SpaceInvaders_Background.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	backgroundBuildingsImg, _, err := ebitenutil.NewImageFromFile("assets/SpaceInvaders_BackgroundBuildings.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	game := Game{
+		BackgroundImg:          backgroundImg,
+		BackgroundBuildingsImg: backgroundBuildingsImg,
+	}
+
+	if err := ebiten.RunGame(&game); err != nil {
 		log.Fatal(err)
 	}
 }
