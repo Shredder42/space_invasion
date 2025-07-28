afa5d83dc27ff46b5695808511067061ca5ec5a3
shredder42 <atbinder42@gmail.com>
2025-07-28 12:13:46 -0500

    basic server running

--- server/main.go
+++ server/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Gamer server is running")
+	})
+
+	log.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
